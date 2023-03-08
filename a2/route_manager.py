4a0fd8aea72238ee2de11fc2e9bac8750e5f6e1f
kokiitagaki <kokiitagaki@uvic.ca>
2023-03-07 17:39:36 -0800

    Assignment 2 is done by Koki Itagaki

--- a2/route_manager.py
+++ a2/route_manager.py
@@ -19,21 +19,14 @@ import argparse
 
 def question1(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd.DataFrame, question: str, graph_type: str) -> None:
     '''
-
     This function performs data processing tasks and generates a visualization in the form of a bar or pie chart for the top 20 airlines with the greatest number of routes to Canada.
     The function saves the results to both a CSV file and a PDF file with a filename that matches the 'question' parameter.
-    :param pd.DataFrame airline_df: The dataframe which contains airlines information
+    :param pd.DataFrame  airline_df: The dataframe which contains airlines information
     :param pd.DataFrame airport_df: The dataframe which contains airports information
     :param pd.DataFrame routes_df: The dataframe which contains routes information
     :param str question: The specific question which you would like this function to solve.
     :param str graph_type: The specific type of the graph which you would like to output into a pdf file
     :return: the program passes the final data frame to another function thus returns nothing
-   
-
-
-
-
-
     '''
     airline_df = airline_df.drop(['airline_country'], axis=1)
     airports_df = airports_df.drop(['airport_name', 'airport_city', 'airport_icao_unique_code', 'airport_altitude'], axis=1)
@@ -43,45 +36,43 @@ def question1(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd
 
     routes_df = routes_df.rename(columns={'route_airline_id': 'airline_id'})
     sol = pd.merge(routes_df, airline_df, on='airline_id', how='inner')
-
     sol = sol.rename(columns={'route_to_airport_id': 'airport_id'})
     sol = pd.merge(sol, airports_df, on='airport_id', how='inner')
-
     sol = remove_space(sol, sol.columns)
-
     sol = sol.groupby(['airline_name', 'airline_icao_unique_code']).size().reset_index(name='size')
     sol = sol.sort_values(by=['size', 'airline_name'], ascending=[False, True]).head(20)
+
+    
+    # Writing the data to CSV file
     with open(f"{question}.csv", mode='w') as output_csv:
         writer = csv.writer(output_csv)
         writer.writerow(['subject', 'statistic'])
         keys = []
         values = []
         for index, row in sol.iterrows():
-            keys.append(row['airline_name'])
+            keys.append(f"{row['airline_name']} ({row['airline_icao_unique_code']})")
             values.append(float(row['size']))
             writer.writerow([f"{row['airline_name']} ({row['airline_icao_unique_code']})", row['size']])
 
-        f = plt.figure(figsize=(10,7))
-        if graph_type == 'bar':
-            plt.bar(keys, values)
-            plt.tick_params(axis='x', which='major', labelsize=8)
-            plt.xticks(rotation=60)
-            plt.subplots_adjust(top=1.0, bottom=0.1)
-            plt.xlabel("Airlines")
-            plt.ylabel("Frequency")
-            plt.title("Top 20 Airlines With The Greatest Number Of Routes To Canada")
-        else:
-            plt.pie(values, labels=keys, autopct='%1.0f%%', labeldistance=1.0, pctdistance=0.5)
-            plt.subplots_adjust(left=0.1)
-            plt.title("Top 20 Airlines With The Greatest Number Of Routes To Canada")
+    # Creating the plot
+    fig, ax = plt.subplots(figsize=(10,7))
+    if graph_type == 'bar':
+        ax.bar(keys, values)
+        ax.tick_params(axis='x', which='major', labelsize=8)
+        ax.set_xticks(range(len(keys)))
+        ax.set_xticklabels(keys, rotation=60, ha='right', fontsize=8)
+        ax.set_xlim(-0.5, len(keys)-0.5)
+        ax.set_ylim(bottom=0)
+        ax.set_xlabel('Airlines')
+        ax.set_ylabel('Frequency')
+        ax.set_title('Top 20 Airlines With The Greatest Number Of Routes To Canada')
+    else:
+        ax.pie(values, labels=keys, autopct='%1.0f%%', labeldistance=1.0, pctdistance=0.5)
+        ax.set_title('Top 20 Airlines With The Greatest Number Of Routes To Canada')
 
-        f.savefig(f"{question}.pdf")
+    # Saving the plot to PDF
+    fig.savefig(f"{question}.pdf")
 
-
-
-
-
- 
 
 
 def question2(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd.DataFrame, question: str, graph_type: str) -> None:
@@ -107,6 +98,7 @@ def question2(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd
     sol = remove_space(sol,sol.columns)
     sol = sol.groupby(['airport_country'], as_index=False).size().sort_values(by=['size', 'airport_country']).head(30)
     
+        # Writing the data to CSV file
     with open(f"{question}.csv", mode='w') as output_csv:
         writer = csv.writer(output_csv)
         writer.writerow(['subject', 'statistic'])
@@ -116,31 +108,27 @@ def question2(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd
             keys.append(row['airport_country'])
             values.append(float(row['size']))
             writer.writerow([row['airport_country'], row['size']])
-
-        fig, ax = plt.subplots(figsize=(10,7))
-        if graph_type == 'bar':
-            ax.bar(keys, values)
-            ax.tick_params(axis='x', which='major', labelsize=8)
-            ax.set_xticks(range(len(keys))) # specify the positions of the tick marks
-            ax.set_xticklabels(keys, rotation=60)
-            ax.set_xlabel('Countries')
-            ax.set_ylabel('Frequency')
-            ax.set_title('Top 30 Countries With The Least Appearances As A Destination Country')
-            fig.subplots_adjust(top=1.0, bottom=0.1)
-        else:
-            ax.pie(values, labels=keys, autopct='%1.0f%%', labeldistance=1.0, pctdistance=0.5)
-            ax.set_title('Top 30 Countries With The Least Appearances As A Destination Country')
-            fig.subplots_adjust(left=0.1)
-
-        fig.savefig(f"{question}.pdf")
+        
+        # Creating the plot
+    fig, ax = plt.subplots(figsize=(10,7))
+    if graph_type == 'bar':
+        ax.bar(keys, values)
+        ax.tick_params(axis='x', which='major', labelsize=8)
+        ax.set_xticks(range(len(keys)))
+        ax.set_xticklabels(keys, rotation=60)
+        ax.set_xlabel('Countries')
+        ax.set_ylabel('Frequency')
+        ax.set_title('Top 30 Countries With The Least Appearances As A Destination Country')
+        fig.subplots_adjust(top=1.0, bottom=0.1)
+    else:
+        ax.pie(values, labels=keys, autopct='%1.0f%%', labeldistance=1.0, pctdistance=0.5)
+        ax.set_title('Top 30 Countries With The Least Appearances As A Destination Country')
+        fig.subplots_adjust(left=0.1)
+    # Saving the plot to PDF
+    fig.savefig(f"{question}.pdf")
     
 
-    
 
-
-
-
-    
 
 def question3(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd.DataFrame, question: str, graph_type: str) -> None:
     '''
@@ -160,32 +148,22 @@ def question3(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd
 
     routes_df = routes_df.rename(columns={'route_airline_id':'airline_id'})
     sol = pd.merge(routes_df, airline_df, on='airline_id', how='left')
-
     sol = sol.rename(columns={'route_to_airport_id':'airport_id'})
     sol = pd.merge(sol, airports_df, on='airport_id', how='left')
-
     sol = remove_space(sol, sol.columns)
     sol = sol.groupby(['airport_name', 'airport_icao_unique_code', 'airport_city', 'airport_country']).size().reset_index(name='size').sort_values(by=['size', 'airport_name'], ascending=[False, True]).head(10)
 
-
-
-
-
-    # Open output CSV file for writing
+        # Writing the data to CSV file
     with open(f"{question}.csv", mode='w') as output_csv:
-        # Initialize writer and write header row
         writer = csv.writer(output_csv)
         writer.writerow(['subject', 'statistic'])
-        # Iterate through dataframe and write rows
         for index, row in sol.iterrows():
             airport_label = f"{row['airport_name']} ({row['airport_icao_unique_code']}), {row['airport_city']}, {row['airport_country']}"
             writer.writerow([airport_label, row['size']])
-
-    # Create keys and values lists
     keys = [f"{row['airport_name']} ({row['airport_icao_unique_code']})" for index, row in sol.iterrows()]
     values = [float(row['size']) for index, row in sol.iterrows()]
 
-    # Create plot and adjust settings
+        # Creating the plot
     fig, ax = plt.subplots(figsize=(10,7))
     if graph_type == 'bar':
         ax.bar(keys, values)
@@ -201,7 +179,7 @@ def question3(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd
         ax.pie(values, labels=labels, autopct='%1.0f%%', labeldistance=1.0, pctdistance=0.5)
         ax.set_title("Top 10 Destination Airports")
 
-    # Save plot to PDF file
+    # Saving the plot to PDF
     fig.savefig(f"{question}.pdf")
 
 
@@ -220,31 +198,26 @@ def question4(airline_df: pd.DataFrame,airports_df: pd.DataFrame, routes_df: pd.
     :param str question: The specific question which you would like this function to solve.
     :param str graph_type: The specific type of the graph which you would like to output into a pdf file
     :return: the program passes the final data frame to another function thus returns nothing
-    '''
+    ''' 
     airports_df = airports_df.drop(['airport_altitude', 'airport_icao_unique_code', 'airport_name'], axis=1)
     routes_df = routes_df.drop(['route_from_aiport_id'], axis=1)
 
     routes_df['route_to_airport_id'] = routes_df['route_to_airport_id'].astype(str)
     airports_df = airports_df.rename(columns={'airport_id':'route_to_airport_id'})
     sol = pd.merge(routes_df, airports_df, on='route_to_airport_id', how='inner') 
-
     sol = remove_space(sol, sol.columns)
     sol = sol.groupby(['airport_city', 'airport_country']).size().reset_index(name='size').sort_values(by=['size', 'airport_city'], ascending=[False, True]).head(15)
     
-    
+        # Writing the data to CSV file
     with open(f"{question}.csv", mode='w') as output_csv:
-        # Initialize writer and write header row
         writer = csv.writer(output_csv)
         writer.writerow(['subject', 'statistic'])
-        # Iterate through dataframe and write rows
         for index, row in sol.iterrows():
             writer.writerow([f"{row['airport_city']}, {row['airport_country']}", row['size']])
-
-    # Create keys and values lists
     keys = [f"{row['airport_city']}, {row['airport_country']}" for index, row in sol.iterrows()]
     values = [float(row['size']) for index, row in sol.iterrows()]
 
-    # Create plot and adjust settings
+    # Creating the plot
     fig, ax = plt.subplots(figsize=(10,7))
     if graph_type == 'bar':
         ax.bar(keys, values)
@@ -260,14 +233,9 @@ def question4(airline_df: pd.DataFrame,airports_df: pd.DataFrame, routes_df: pd.
         ax.pie(values, labels=keys, autopct='%1.0f%%', labeldistance=1.0, pctdistance=0.5)
         ax.set_title("Top 15 Destination Cities")
 
-    # Save plot to PDF file
+    # Saving the plot to PDF
     fig.savefig(f"{question}.pdf")
     
-
-
-
-    
-
 
 
 
@@ -292,25 +260,21 @@ def question5(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd
 
     sol = pd.merge(routes_df, airports_df_canada, on='route_to_airport_id', how='inner')
     sol = sol.rename(columns = {'airport_altitude':'to_airport_altitude', 'airport_icao_unique_code':'to_airport_icao_unique_code'})
-
     airports_df_canada = airports_df_canada.rename(columns = {'route_to_airport_id': 'route_from_aiport_id'})
     sol = pd.merge(sol, airports_df_canada, on='route_from_aiport_id', how='inner')
     sol = sol.rename(columns = {'airport_altitude':'from_airport_altitude', 'airport_icao_unique_code':'from_airport_icao_unique_code'})
-
-    sol['diff'] = -1.0  # creating a new column to store the difference and initializing it to -1 since the calculated difference >= 0
+     # Calculate the absolute difference in altitude between the 'to' and 'from' airports and remove any extra spaces from the dataframe
     sol['diff'] = abs(pd.to_numeric(sol['to_airport_altitude'], errors='coerce') - pd.to_numeric(sol['from_airport_altitude'], errors='coerce'))
   
     sol = remove_space(sol,sol.columns)
     sol = sol.sort_values(by=['diff', 'to_airport_icao_unique_code', 'from_airport_icao_unique_code'], ascending=[False, True, True]) 
     
-    
+     # Writing the data to CSV file
     with open(f"{question}.csv", mode='w') as output_csv:
         # Write header row
         output_csv.write("subject,statistic\n")
-        # Initialize keys and values lists
         keys = []
         values = []
-        # Write rows and populate keys and values lists
         uniques = clear_duplicates(sol)
         for i in range(10):
             key = f"{uniques[i][0]}-{uniques[i][1]}"
@@ -319,34 +283,24 @@ def question5(airline_df: pd.DataFrame, airports_df: pd.DataFrame, routes_df: pd
             values.append(value)
             output_csv.write(f"{key},{value}\n")
 
-        # Create plot and adjust settings
-        fig, ax = plt.subplots(figsize=(10,7))
-        if graph_type == 'bar':
-            ax.bar(keys, values)
-            ax.tick_params(axis='x', which='major', labelsize=8)
-            ax.set_xticks(range(len(keys)))
-            ax.set_xticklabels(keys, rotation=60)
-            ax.set_xlabel('Routes')
-            ax.set_ylabel('Difference In Altitude')
-            ax.set_title("Top 10 Canadian Routes With The Greatest Difference In Altitude")
-            fig.subplots_adjust(top=1.0, bottom=0.1)
-        else:
-            ax.pie(values, labels=keys, autopct='%1.0f%%', labeldistance=1.0, pctdistance=0.5)
-            ax.set_title("Top 10 Canadian Routes With The Greatest Difference In Altitude")
-            fig.subplots_adjust(left=0.1)
+     # Create plot and adjust settings
+    fig, ax = plt.subplots(figsize=(10,7))
+    if graph_type == 'bar':
+        ax.bar(keys, values)
+        ax.tick_params(axis='x', which='major', labelsize=8)
+        ax.set_xticks(range(len(keys)))
+        ax.set_xticklabels(keys, rotation=60)
+        ax.set_xlabel('Routes')
+        ax.set_ylabel('Difference In Altitude')
+        ax.set_title("Top 10 Canadian Routes With The Greatest Difference In Altitude")
+        fig.subplots_adjust(top=1.0, bottom=0.1)
+    else:
+        ax.pie(values, labels=keys, autopct='%1.0f%%', labeldistance=1.0, pctdistance=0.5)
+        ax.set_title("Top 10 Canadian Routes With The Greatest Difference In Altitude")
+        fig.subplots_adjust(left=0.1)
 
-        # Save plot to PDF file
-        fig.savefig(f"{question}.pdf")
- 
-
-
-
-
-
-
-
-
-
+    # Save plot to PDF file
+    fig.savefig(f"{question}.pdf")
 
 
 
@@ -370,7 +324,6 @@ def clear_duplicates(df: pd.DataFrame) -> tuple:
     
 
 
-
 def remove_space(df: pd.DataFrame, columns: str) -> pd.DataFrame: 
     '''
    Removes any whitespace characters from the beginning of the string.
@@ -384,11 +337,9 @@ def remove_space(df: pd.DataFrame, columns: str) -> pd.DataFrame:
 
 
 
-
-
-
-
 def main():
+
+    # Creating the argument parser and adding command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--AIRLINES', type=str, required=True, help='Path to airlines YAML file')
     parser.add_argument('--AIRPORTS', type=str, required=True, help='Path to airports YAML file')
@@ -398,6 +349,8 @@ def main():
 
     args = parser.parse_args()
 
+
+    #Loading each data into dataframes
     with open(args.AIRLINES) as f:
         airline_df = pd.DataFrame(yaml.safe_load(f)['airlines'])
 
@@ -409,6 +362,8 @@ def main():
 
     question = args.QUESTION
     graph_type = args.GRAPH_TYPE
+
+    # Dictionary of functions for each question
     question_funcs = {
     'q1': question1,
     'q2': question2,
@@ -421,5 +376,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
 
